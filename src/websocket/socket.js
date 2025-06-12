aeb30a76f430576ce9148c07cae19e0c306bb167
charan1147 <charanc1147@gmail.com>
2025-06-12 19:53:32 +0530

    4 commit

--- src/websocket/socket.js
+++ src/websocket/socket.js
@@ -1,7 +1,7 @@
 // src/websocket/socket.js
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_BASE_URL, {
+const socket = io(import.meta.env.VITE_SOCKET_URL, {
   withCredentials: true,
 });
 
